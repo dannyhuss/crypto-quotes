8d6c5d2d41142a973b4e87ff8976a375d3d9f650
Danny Huss <Danny.Huss@protonmail.com>
2020-07-09 14:13:52 -0500

    initial commit

---
+++ src/crypto.cpp
@@ -0,0 +1,15 @@
+/**
+ *  CRYPTO-QUOTES main application file using OOP
+ *
+ *  Author: Danny Huss
+ *  Email: danny.huss@protonmail.com
+ *  Created: 2020-07-09
+**/
+
+
+
+int main(void) {
+
+    //model = new QuoteLibrary("data/quotes.dat");
+
+}
